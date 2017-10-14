ac9b9b2496c9cfd3a636e0f5f74dac872d09a540
Steven Haye <sphred5@gmail.com>
2017-10-13 17:06:37 -0700

    first commit

---
+++ data.py
@@ -0,0 +1,21 @@
+def Articles():
+    articles = [
+    {'id' : 1,
+    'title' : 'Article one',
+    'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor,',
+      'author' : 'Steven Haye',
+      'create_date' : '04-25-2017'
+    },
+    {'id' : 2,
+    'title' : 'Article two',
+    'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor,',
+      'author' : 'John doe',
+      'create_date' : '04-25-2017'
+    },
+    {'id' : 3,
+    'title' : 'Article three',
+    'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor,',
+    'author' : 'Steven Haye',
+    'create_date' : '04-25-2017'}
+    ]
+    return articles
